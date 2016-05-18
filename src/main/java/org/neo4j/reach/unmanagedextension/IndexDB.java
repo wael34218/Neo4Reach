3ff1d606f88c603f4ebcb6db6dc0e6231eb9a34b
Wael Farhan <wael34218@gmail.com>
2016-05-17 19:27:35 -0700

    Fix indentation

--- src/main/java/org/neo4j/reach/unmanagedextension/IndexDB.java
+++ src/main/java/org/neo4j/reach/unmanagedextension/IndexDB.java
@@ -20,106 +20,112 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class IndexDB {
 	private static IndexDB instance = null;
-	
+
 	public GraphDatabaseService connection;
 	public Hashtable<Long, Long> mapping = new Hashtable<Long, Long>();
 	static String index_path = "data/databases/reachindex";
-	
+
 	static Hashtable<Long, Integer> S_out = new Hashtable<Long, Integer>();
 	static Hashtable<Long, Integer> S_in = new Hashtable<Long, Integer>();
 	static Hashtable<Long, Boolean> Vorder = new Hashtable<Long, Boolean>();
 	static Result result;
 	static ResourceIterator<Node> nodes;
-	
-	protected IndexDB(){ }
-	
+
+	protected IndexDB() {
+	}
+
 	public static IndexDB getInstance() {
-		if(instance == null){
+		if (instance == null) {
 			System.out.println("Create new instance");
 			instance = new IndexDB();
-			instance.connection = new GraphDatabaseFactory().newEmbeddedDatabase(new File(index_path));
+			instance.connection = new GraphDatabaseFactory()
+					.newEmbeddedDatabase(new File(index_path));
 			instance.loadIndex();
-		}else{
+		} else {
 			System.out.println("Instance already created");
 		}
 		return instance;
-    }
-	
-	public void loadIndex(){
+	}
+
+	public void loadIndex() {
 		result = this.connection.execute("match (n) return n");
-        nodes = result.columnAs("n");
-        Node n;
-        Set<Long> temp;
-        this.mapping = new Hashtable<Long, Long>();
-        
-        while(nodes.hasNext()){
-            n = nodes.next();
-            temp = fromString((String) n.getProperty("containing"));
-            for(Long i : temp){
-            	instance.mapping.put(i, n.getId());
-            }
-        }
-    }
-	
+		nodes = result.columnAs("n");
+		Node n;
+		Set<Long> temp;
+		this.mapping = new Hashtable<Long, Long>();
+
+		while (nodes.hasNext()) {
+			n = nodes.next();
+			temp = fromString((String) n.getProperty("containing"));
+			for (Long i : temp) {
+				instance.mapping.put(i, n.getId());
+			}
+		}
+	}
+
 	public static Set<Long> fromString(String string) {
-        Set<Long> result = new HashSet<Long>();
-        if(string.equals("[]")){
-            return result;
-        }
-        
-        String[] strings = string.replace("[", "").replace("]", "").split(", ");
-        for (int i = 0; i < strings.length; i++) {
-          result.add(Long.parseLong((strings[i])));
-        }
-        return result;
-    }
-	
-	public boolean empty(){
+		Set<Long> result = new HashSet<Long>();
+		if (string.equals("[]")) {
+			return result;
+		}
+
+		String[] strings = string.replace("[", "").replace("]", "").split(", ");
+		for (int i = 0; i < strings.length; i++) {
+			result.add(Long.parseLong((strings[i])));
+		}
+		return result;
+	}
+
+	public boolean empty() {
 		result = connection.execute("MATCH (n) RETURN count(n) as c;");
 		ResourceIterator<Long> count = result.columnAs("c");
 		Long nodes_total = (Long) count.next();
-		System.out.println("= Nodes = : "+nodes_total);
+		System.out.println("= Nodes = : " + nodes_total);
 		return (nodes_total == 0);
 	}
-	
-    /////////////////////////////////////////////////////////////////
-    /////////////////   ReIndex
-    /////////////////////////////////////////////////////////////////
-	public void reIndex(GraphDatabaseService database){
+
+	// ///////////////////////////////////////////////////////////////
+	// /////////////// ReIndex
+	// ///////////////////////////////////////////////////////////////
+	public void reIndex(GraphDatabaseService database) {
 		connection.shutdown();
-    	GraphDatabaseService indexDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File(index_path));
+		GraphDatabaseService indexDb = new GraphDatabaseFactory()
+				.newEmbeddedDatabase(new File(index_path));
 		buildSCC(database, indexDb);
 		decideVertixLevel(indexDb);
 		butterfly(indexDb);
 		System.out.println("Indexing done.");
 		indexDb.shutdown();
-		this.connection = new GraphDatabaseFactory().newEmbeddedDatabase(new File(index_path));
+		this.connection = new GraphDatabaseFactory()
+				.newEmbeddedDatabase(new File(index_path));
 		this.loadIndex();
 	}
-	
-	//////////////////////////////////////
-	//   BuildSCC - Part 2
-	//////////////////////////////////////
-	public static void buildSCC(GraphDatabaseService graphDb, GraphDatabaseService indexDb){
+
+	// ////////////////////////////////////
+	// BuildSCC - Part 2
+	// ////////////////////////////////////
+	public static void buildSCC(GraphDatabaseService graphDb,
+			GraphDatabaseService indexDb) {
 		// Formulate SCC using Kosaraju's algorithm
 		Hashtable<Long, Boolean> visited = new Hashtable<Long, Boolean>();
 		Stack<Node> scc_stack = new Stack<Node>();
 		Stack<Node> dfs_stack = new Stack<Node>();
 		Hashtable<Long, Set<Node>> scc = new Hashtable<Long, Set<Node>>();
-		
+
 		// Kosaraju's step 1: DFS
 		Result result = graphDb.execute("match (n) return n");
 		ResourceIterator<Node> nodes = result.columnAs("n");
-		while(nodes.hasNext()){
+		while (nodes.hasNext()) {
 			dfs_stack.push(nodes.next());
-			while(!dfs_stack.empty()){
+			while (!dfs_stack.empty()) {
 				Node node = (Node) dfs_stack.pop();
-				if(visited.containsKey(node.getId())){
+				if (visited.containsKey(node.getId())) {
 					continue;
 				}
 				visited.put(node.getId(), false);
-				Iterable<Relationship> rels = node.getRelationships(Direction.OUTGOING);
-				for(Relationship rel : rels){
+				Iterable<Relationship> rels = node
+						.getRelationships(Direction.OUTGOING);
+				for (Relationship rel : rels) {
 					dfs_stack.push(rel.getEndNode());
 				}
 				scc_stack.push(node);
@@ -127,34 +133,37 @@ public class IndexDB {
 		}
 
 		// Kosaraju's step 2: Reversed graph DFS
-		try (Transaction tindex = indexDb.beginTx(); Transaction tgraph = graphDb.beginTx()) {
-			while(!scc_stack.empty()){
+		try (Transaction tindex = indexDb.beginTx();
+				Transaction tgraph = graphDb.beginTx()) {
+			while (!scc_stack.empty()) {
 				Node root = scc_stack.pop();
-				if(visited.get(root.getId())){
+				if (visited.get(root.getId())) {
 					continue;
 				}
 				dfs_stack.push(root);
 				Node sccNode = indexDb.createNode();
-	
+
 				Set<Node> component = new HashSet<Node>();
-				while(!dfs_stack.empty()){
+				while (!dfs_stack.empty()) {
 					Node node = (Node) dfs_stack.pop();
-					if(visited.get(node.getId())){
+					if (visited.get(node.getId())) {
 						continue;
 					}
 					visited.put(node.getId(), true);
-	
+
 					component.add(node);
 					instance.mapping.put(node.getId(), sccNode.getId());
-					Iterable<Relationship> rels = node.getRelationships(Direction.INCOMING);
-					for(Relationship rel : rels){
+					Iterable<Relationship> rels = node
+							.getRelationships(Direction.INCOMING);
+					for (Relationship rel : rels) {
 						dfs_stack.push(rel.getStartNode());
 					}
 				}
-	
-				// Store SCC node in the index database with "containing" property: a serialized string of node ids.
+
+				// Store SCC node in the index database with "containing"
+				// property: a serialized string of node ids.
 				Set<Long> containing = new HashSet<Long>();
-				for(Node c : component){
+				for (Node c : component) {
 					containing.add(c.getId());
 				}
 				sccNode.setProperty("containing", containing.toString());
@@ -162,12 +171,12 @@ public class IndexDB {
 				scc.put(sccNode.getId(), component);
 			}
 			tindex.success();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		// Add relations
-		result = graphDb.execute( "match ()-[r]->() return r" );
+		result = graphDb.execute("match ()-[r]->() return r");
 		ResourceIterator<Relationship> relations = result.columnAs("r");
 		Relationship rel;
 		Long startSccId, endSccId;
@@ -175,51 +184,57 @@ public class IndexDB {
 		Hashtable<String, Boolean> added_relations = new Hashtable<String, Boolean>();
 
 		try (Transaction tindex = indexDb.beginTx()) {
-			while(relations.hasNext()){
+			while (relations.hasNext()) {
 				rel = relations.next();
 				startSccId = instance.mapping.get(rel.getStartNode().getId());
 				endSccId = instance.mapping.get(rel.getEndNode().getId());
-				if(!startSccId.equals(endSccId) && !added_relations.containsKey(startSccId+"-"+endSccId)){
-					added_relations.put(startSccId+"-"+endSccId, true);
-	
+				if (!startSccId.equals(endSccId)
+						&& !added_relations.containsKey(startSccId + "-"
+								+ endSccId)) {
+					added_relations.put(startSccId + "-" + endSccId, true);
+
 					// Get Start SCC node
-					result = indexDb.execute( "match (s) WHERE ID(s) = "+startSccId+" return s" );
-					ResourceIterator<Node> startSccResults = result.columnAs("s");
+					result = indexDb.execute("match (s) WHERE ID(s) = "
+							+ startSccId + " return s");
+					ResourceIterator<Node> startSccResults = result
+							.columnAs("s");
 					startScc = startSccResults.next();
-	
+
 					// Get End SCC node
-					result = indexDb.execute( "match (s) WHERE ID(s) = "+endSccId+" return s" );
+					result = indexDb.execute("match (s) WHERE ID(s) = "
+							+ endSccId + " return s");
 					ResourceIterator<Node> endSccResults = result.columnAs("s");
 					endScc = endSccResults.next();
-	
+
 					// Store Relation
-					Relationship relationship = startScc.createRelationshipTo(endScc, rel.getType());
+					Relationship relationship = startScc.createRelationshipTo(
+							endScc, rel.getType());
 					relationship.setProperty("message", "Some Information");
 				}
 			}
 			tindex.success();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	//////////////////////////////////////
-	//   Decide Vertex Level - Part 7.1
-	//////////////////////////////////////
-	private void decideVertixLevel(GraphDatabaseService indexDb){
+
+	// ////////////////////////////////////
+	// Decide Vertex Level - Part 7.1
+	// ////////////////////////////////////
+	private void decideVertixLevel(GraphDatabaseService indexDb) {
 		Long order_level = (long) 1;
 
 		try (Transaction tindex = indexDb.beginTx()) {
 			result = indexDb.execute("MATCH (n) RETURN count(n) as c");
 			ResourceIterator<Long> count_query = result.columnAs("c");
 			Long nodes_total = (Long) count_query.next();
-	
-			while(order_level <= nodes_total){
+
+			while (order_level <= nodes_total) {
 				result = indexDb.execute("MATCH (n) RETURN n");
 				nodes = result.columnAs("n");
-				while(nodes.hasNext()){
+				while (nodes.hasNext()) {
 					Node node = nodes.next();
-					if(Vorder.containsKey(node.getId())){
+					if (Vorder.containsKey(node.getId())) {
 						S_out.put(node.getId(), -1);
 						S_in.put(node.getId(), -1);
 						continue;
@@ -228,7 +243,8 @@ public class IndexDB {
 					findIn(node);
 				}
 				Long maxNodeID = findMax();
-				result = indexDb.execute("MATCH (n) WHERE ID(n)="+maxNodeID+" SET n.order="+order_level);
+				result = indexDb.execute("MATCH (n) WHERE ID(n)=" + maxNodeID
+						+ " SET n.order=" + order_level);
 				Vorder.put(maxNodeID, true);
 				order_level++;
 			}
@@ -236,19 +252,21 @@ public class IndexDB {
 		}
 	}
 
-	private static Long findMax(){
+	private static Long findMax() {
 		// Loop through nodes and find the maximum f(v,G) =
-		// (|Sin(v,G)|.|Sout(v,G)| + |Sin(v,G)| + |Sout(v,G)|) / (|Sin(v,G)| + |Sout(v,G)|)
+		// (|Sin(v,G)|.|Sout(v,G)| + |Sin(v,G)| + |Sout(v,G)|) / (|Sin(v,G)| +
+		// |Sout(v,G)|)
 		Long nodeID = (long) 0;
 		float max_cost = -1;
 		float cost;
-		for(Long k : S_out.keySet()){
-			if(S_in.get(k) + S_out.get(k) == 0){
+		for (Long k : S_out.keySet()) {
+			if (S_in.get(k) + S_out.get(k) == 0) {
 				cost = 0;
-			}else{
-				cost = (S_in.get(k)*S_out.get(k) + S_in.get(k) + S_out.get(k)) / (S_in.get(k) + S_out.get(k));
+			} else {
+				cost = (S_in.get(k) * S_out.get(k) + S_in.get(k) + S_out.get(k))
+						/ (S_in.get(k) + S_out.get(k));
 			}
-			if(cost > max_cost){
+			if (cost > max_cost) {
 				nodeID = k;
 				max_cost = cost;
 			}
@@ -256,15 +274,15 @@ public class IndexDB {
 		return nodeID;
 	}
 
-	private static int findIn(Node node){
-		if(S_in.containsKey(node.getId())){
+	private static int findIn(Node node) {
+		if (S_in.containsKey(node.getId())) {
 			return S_in.get(node.getId());
 		}
 
 		int in = 0;
 		Iterable<Relationship> rels = node.getRelationships(Direction.INCOMING);
-		for(Relationship rel : rels){
-			if(Vorder.containsKey(rel.getStartNode().getId())){
+		for (Relationship rel : rels) {
+			if (Vorder.containsKey(rel.getStartNode().getId())) {
 				continue;
 			}
 			in += findIn(rel.getStartNode()) + 1;
@@ -273,15 +291,15 @@ public class IndexDB {
 		return in;
 	}
 
-	private static int findOut(Node node){
-		if(S_out.containsKey(node.getId())){
+	private static int findOut(Node node) {
+		if (S_out.containsKey(node.getId())) {
 			return S_out.get(node.getId());
 		}
 
 		int out = 0;
 		Iterable<Relationship> rels = node.getRelationships(Direction.OUTGOING);
-		for(Relationship rel : rels){
-			if(Vorder.containsKey(rel.getEndNode().getId())){
+		for (Relationship rel : rels) {
+			if (Vorder.containsKey(rel.getEndNode().getId())) {
 				continue;
 			}
 			out += findOut(rel.getEndNode()) + 1;
@@ -289,11 +307,11 @@ public class IndexDB {
 		S_out.put(node.getId(), out);
 		return out;
 	}
-	
-	//////////////////////////////////////
-	//   Butterfly - Part 7.2
-	//////////////////////////////////////
-	public void butterfly(GraphDatabaseService indexDb){
+
+	// ////////////////////////////////////
+	// Butterfly - Part 7.2
+	// ////////////////////////////////////
+	public void butterfly(GraphDatabaseService indexDb) {
 		Hashtable<Long, Set<Long>> l_out = new Hashtable<Long, Set<Long>>();
 		Hashtable<Long, Set<Long>> l_in = new Hashtable<Long, Set<Long>>();
 		Hashtable<Long, Boolean> visited = new Hashtable<Long, Boolean>();
@@ -301,104 +319,108 @@ public class IndexDB {
 		try (Transaction tindex = indexDb.beginTx()) {
 			result = indexDb.execute("MATCH (n) RETURN n ORDER BY n.order");
 			nodes = result.columnAs("n");
-			while(nodes.hasNext()){
+			while (nodes.hasNext()) {
 				Queue<Node> bfs_queue = new LinkedList<Node>();
 				ArrayList<Node> b_plus = new ArrayList<Node>();
 				ArrayList<Node> b_minus = new ArrayList<Node>();
-	
+
 				Node node = nodes.next();
 				visited.put(node.getId(), true);
-	
+
 				// Find B Plus
 				bfs_queue.add(node);
-				while(!bfs_queue.isEmpty()){
+				while (!bfs_queue.isEmpty()) {
 					Node bn = bfs_queue.remove();
-					Iterable<Relationship> rels = bn.getRelationships(Direction.OUTGOING);
-					for(Relationship rel : rels){
-						if(visited.containsKey(rel.getEndNode().getId())){
+					Iterable<Relationship> rels = bn
+							.getRelationships(Direction.OUTGOING);
+					for (Relationship rel : rels) {
+						if (visited.containsKey(rel.getEndNode().getId())) {
 							// Consider this node is deleted from the graph
 							continue;
 						}
-						//if((Long) bn.getProperty("order") > (Long) rel.getEndNode().getProperty("order")){
+						// if((Long) bn.getProperty("order") > (Long)
+						// rel.getEndNode().getProperty("order")){
 						bfs_queue.add(rel.getEndNode());
 						b_plus.add(rel.getEndNode());
-						//}
+						// }
 					}
 				}
-	
+
 				// Find B Minus
 				bfs_queue.add(node);
-				while(!bfs_queue.isEmpty()){
+				while (!bfs_queue.isEmpty()) {
 					Node bn = bfs_queue.remove();
-					Iterable<Relationship> rels = bn.getRelationships(Direction.INCOMING);
-	
-					for(Relationship rel : rels){
-						if(visited.containsKey(rel.getStartNode().getId())){
+					Iterable<Relationship> rels = bn
+							.getRelationships(Direction.INCOMING);
+
+					for (Relationship rel : rels) {
+						if (visited.containsKey(rel.getStartNode().getId())) {
 							// Consider this node is deleted from the graph
 							continue;
 						}
-	
-						//if((Long) bn.getProperty("order") < (Long) rel.getStartNode().getProperty("order")){
+
+						// if((Long) bn.getProperty("order") < (Long)
+						// rel.getStartNode().getProperty("order")){
 						bfs_queue.add(rel.getStartNode());
 						b_minus.add(rel.getStartNode());
-						//}
+						// }
 					}
 				}
-	
+
 				// Update l_in
-				for(Node u : b_plus){
+				for (Node u : b_plus) {
 					Set<Long> l_out_temp = new HashSet<Long>();
 					Set<Long> l_in_temp = new HashSet<Long>();
-	
-					if(l_out.containsKey(node.getId())){
+
+					if (l_out.containsKey(node.getId())) {
 						l_out_temp = l_out.get(node.getId());
 					}
-					if(l_in.containsKey(u.getId())){
+					if (l_in.containsKey(u.getId())) {
 						l_in_temp = l_in.get(u.getId());
 					}
-	
+
 					l_out_temp.retainAll(l_in_temp);
-					if(l_out_temp.size() == 0){
+					if (l_out_temp.size() == 0) {
 						l_in_temp.add(node.getId());
 						l_in.put(u.getId(), l_in_temp);
 					}
 				}
-	
+
 				// Update l_out
-				for(Node u : b_minus){
+				for (Node u : b_minus) {
 					Set<Long> l_out_temp = new HashSet<Long>();
 					Set<Long> l_in_temp = new HashSet<Long>();
-	
-					if(l_out.containsKey(u.getId())){
+
+					if (l_out.containsKey(u.getId())) {
 						l_out_temp = l_out.get(u.getId());
 					}
-					if(l_in.containsKey(node.getId())){
+					if (l_in.containsKey(node.getId())) {
 						l_in_temp = l_in.get(node.getId());
 					}
-	
+
 					l_in_temp.retainAll(l_out_temp);
-					if(l_in_temp.size() == 0){
+					if (l_in_temp.size() == 0) {
 						l_out_temp.add(node.getId());
 						l_out.put(u.getId(), l_out_temp);
 					}
 				}
 			}
-	
+
 			// Store l_out and l_in in Neo4j
 			result = indexDb.execute("MATCH (n) RETURN n ORDER BY n.order");
 			nodes = result.columnAs("n");
-			while(nodes.hasNext()){
+			while (nodes.hasNext()) {
 				Node node = nodes.next();
 				Set<Long> l_out_temp = new HashSet<Long>();
 				Set<Long> l_in_temp = new HashSet<Long>();
-	
-				if(l_out.containsKey(node.getId())){
+
+				if (l_out.containsKey(node.getId())) {
 					l_out_temp = l_out.get(node.getId());
 				}
-				if(l_in.containsKey(node.getId())){
+				if (l_in.containsKey(node.getId())) {
 					l_in_temp = l_in.get(node.getId());
 				}
-	
+
 				node.setProperty("l_out", l_out_temp.toString());
 				node.setProperty("l_in", l_in_temp.toString());
 			}
--- src/main/java/org/neo4j/reach/unmanagedextension/MyService.java
+++ src/main/java/org/neo4j/reach/unmanagedextension/MyService.java
@@ -12,91 +12,97 @@ import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Result;
 
-
 @Path("/reachability")
 public class MyService {
-    static Result result;
-    static ResourceIterator<Node> nodes;
-    
-    private final GraphDatabaseService database;
-    private final IndexDB indexDb;
-    
-    
-    /////////////////////////////////////////////////////////////////
-    /////////////////   Initialize
-    /////////////////////////////////////////////////////////////////
-    public MyService(@Context GraphDatabaseService database){
-        this.database = database;
-        new IndexDB();
+	static Result result;
+	static ResourceIterator<Node> nodes;
+
+	private final GraphDatabaseService database;
+	private final IndexDB indexDb;
+
+	// ///////////////////////////////////////////////////////////////
+	// /////////////// Initialize
+	// ///////////////////////////////////////////////////////////////
+	public MyService(@Context GraphDatabaseService database) {
+		this.database = database;
+		new IndexDB();
 		this.indexDb = IndexDB.getInstance();
-		if(indexDb.empty()){
+		if (indexDb.empty()) {
 			indexDb.reIndex(database);
-		}else{
+		} else {
 			indexDb.loadIndex();
 		}
-    }
+	}
 
-    /////////////////////////////////////////////////////////////////
-    /////////////////   Rest Calls
-    /////////////////////////////////////////////////////////////////
-    @GET
-    @Path( "/noindex/source/{s}/target/{t}" )
-    public String graphReachability(@PathParam("s") Long s, @PathParam("t") Long t){
-        result = this.database.execute( "MATCH p=((s)-[*..50]->(t)) WHERE ID(s)="+s+" AND ID(t)="+t+" RETURN length(p) LIMIT 1" );
-        ResourceIterator<Node> answer = result.columnAs("s");
-        if(answer.hasNext()){
-        	return "true\n";
-        }else{
-        	return "false\n";
-        }
-    }
-    
-    @GET
-    @Path( "/source/{s}/target/{t}" )
-    public String indexReachability(@PathParam("s") Long s, @PathParam("t") Long t){
-        Long si = indexDb.mapping.get(s);
-        Long ti = indexDb.mapping.get(t);
-        Node startScc = null;
-        Node endScc = null;
+	// ///////////////////////////////////////////////////////////////
+	// /////////////// Rest Calls
+	// ///////////////////////////////////////////////////////////////
+	@GET
+	@Path("/noindex/source/{s}/target/{t}")
+	public String graphReachability(@PathParam("s") Long s,
+			@PathParam("t") Long t) {
+		result = this.database
+				.execute("MATCH p=((s)-[*..50]->(t)) WHERE ID(s)=" + s
+						+ " AND ID(t)=" + t + " RETURN length(p) LIMIT 1");
+		ResourceIterator<Node> answer = result.columnAs("s");
+		if (answer.hasNext()) {
+			return "true\n";
+		} else {
+			return "false\n";
+		}
+	}
 
-        // Get Start SCC node
-        try{
-	        result = indexDb.connection.execute("match (s) WHERE ID(s) = "+si+" return s");
-	        ResourceIterator<Node> startSccResults = result.columnAs("s");
-	        startScc = startSccResults.next();
-        }catch(RuntimeException e){
-        	return "Source: " + s + " ==> " + si + " NOT FOUND";
-        }
+	@GET
+	@Path("/source/{s}/target/{t}")
+	public String indexReachability(@PathParam("s") Long s,
+			@PathParam("t") Long t) {
+		Long si = indexDb.mapping.get(s);
+		Long ti = indexDb.mapping.get(t);
+		Node startScc = null;
+		Node endScc = null;
 
-        // Get End SCC node
-        try{
-	        result = indexDb.connection.execute( "match (t) WHERE ID(t) = "+ti+" return t" );
-	        ResourceIterator<Node> endSccResults = result.columnAs("t");
-	        endScc = endSccResults.next();
-	    }catch(RuntimeException e){
-	    	return "Target: " + t + " ==> " + ti + " NOT FOUND";
-	    }
-        
-        Set<Long> l_out_temp = IndexDB.fromString((String) startScc.getProperty("l_out"));
-        Set<Long> l_in_temp = IndexDB.fromString((String) endScc.getProperty("l_in"));
-        l_out_temp.add(startScc.getId());
-        l_in_temp.add(endScc.getId());
+		// Get Start SCC node
+		try {
+			result = indexDb.connection.execute("match (s) WHERE ID(s) = " + si
+					+ " return s");
+			ResourceIterator<Node> startSccResults = result.columnAs("s");
+			startScc = startSccResults.next();
+		} catch (RuntimeException e) {
+			return "Source: " + s + " ==> " + si + " NOT FOUND";
+		}
 
-        l_out_temp.retainAll(l_in_temp);
-        
-        String res;
-        if(l_out_temp.size() == 0){
-            res = "false\n";
-        }else{
-            res = "true\n";
-        }
-        return res;
-    }
+		// Get End SCC node
+		try {
+			result = indexDb.connection.execute("match (t) WHERE ID(t) = " + ti
+					+ " return t");
+			ResourceIterator<Node> endSccResults = result.columnAs("t");
+			endScc = endSccResults.next();
+		} catch (RuntimeException e) {
+			return "Target: " + t + " ==> " + ti + " NOT FOUND";
+		}
 
-    @GET
-    @Path("/helloworld")
-    public String helloWorld() {
-        return "Hello World!\n";
-    }
+		Set<Long> l_out_temp = IndexDB.fromString((String) startScc
+				.getProperty("l_out"));
+		Set<Long> l_in_temp = IndexDB.fromString((String) endScc
+				.getProperty("l_in"));
+		l_out_temp.add(startScc.getId());
+		l_in_temp.add(endScc.getId());
+
+		l_out_temp.retainAll(l_in_temp);
+
+		String res;
+		if (l_out_temp.size() == 0) {
+			res = "false\n";
+		} else {
+			res = "true\n";
+		}
+		return res;
+	}
+
+	@GET
+	@Path("/helloworld")
+	public String helloWorld() {
+		return "Hello World!\n";
+	}
 
 }
