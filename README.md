387075c5ce772fbddfa6d78206448292e67b556f
Wael Farhan <wael34218@gmail.com>
2016-05-17 18:50:32 -0700

    Fix reindexing connection instance

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
-Sample Neo4j unmanaged extension
+Neo4j Reachability Index
 ================================
 
 This is an unmanaged extension. 
 
-1. Build it: 
+1. Build it using: 
 
         mvn clean package
 
-2. Copy target/unmanaged-extension-template-1.0.jar to the plugins/ directory of your Neo4j server.
+2. Copy target/neo4reach-1.0.jar to the plugins/ directory of your Neo4j server.
 
 3. Configure Neo4j by adding a line to conf/neo4j-server.properties:
 
-        org.neo4j.server.thirdparty_jaxrs_classes=org.neo4j.example.unmanagedextension=/example
+        dbms.unmanaged_extension_classes=org.neo4j.reach.unmanagedextension=/reach
 
 4. Start Neo4j server.
 
 5. Query it over HTTP:
 
-        curl http://localhost:7474/example/service/helloworld
+        curl http://localhost:7474/reach/reachability/source/{source node id}/target/{target node id}
 
+You could also compare it with reachability query without using the index using:
+
+        curl http://localhost:7474/reach/reachability/noindex/source/{source node id}/target/{target node id}
+
+
+This work is based on:
+
+Zhu, Andy Diwen, et al. "Reachability queries on large dynamic graphs: a total order approach." Proceedings of the 2014 ACM SIGMOD international conference on Management of data. ACM, 2014.
--- src/main/java/org/neo4j/reach/unmanagedextension/MyService.java
+++ src/main/java/org/neo4j/reach/unmanagedextension/MyService.java
@@ -1,30 +1,20 @@
 package org.neo4j.reach.unmanagedextension;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Hashtable;
 import java.util.Set;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Result;
-import org.neo4j.io.fs.FileUtils;
 
 
 @Path("/reachability")
 public class MyService {
-    static String index_path = "/Users/wael/Documents/Neo4j/drwhoindex";
     static Result result;
     static ResourceIterator<Node> nodes;
     
@@ -35,33 +25,30 @@ public class MyService {
     /////////////////////////////////////////////////////////////////
     /////////////////   Initialize
     /////////////////////////////////////////////////////////////////
-    public MyService(@Context GraphDatabaseService database)
-    {
-        boolean reset = false;
-        // Delete index database if it exists and start over.
-        
+    public MyService(@Context GraphDatabaseService database){
         this.database = database;
-        File index = new File(index_path);
-        if(reset){
-            try {
-                FileUtils.deleteRecursively(index);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        this.indexDb = new IndexDB().getInstance();
+        new IndexDB();
+		this.indexDb = IndexDB.getInstance();
+		if(indexDb.empty()){
+			indexDb.reIndex(database);
+		}else{
+			indexDb.loadIndex();
+		}
     }
 
     /////////////////////////////////////////////////////////////////
     /////////////////   Rest Calls
     /////////////////////////////////////////////////////////////////
     @GET
-    @Produces( MediaType.TEXT_PLAIN )
     @Path( "/noindex/source/{s}/target/{t}" )
-    public Response graphReachability(Long s, Long t){
+    public String graphReachability(@PathParam("s") Long s, @PathParam("t") Long t){
         result = this.database.execute( "MATCH p=((s)-[*..50]->(t)) WHERE ID(s)="+s+" AND ID(t)="+t+" RETURN length(p) LIMIT 1" );
         ResourceIterator<Node> answer = result.columnAs("s");
-        return Response.status( Status.OK ).entity(answer.hasNext()).build();
+        if(answer.hasNext()){
+        	return "true\n";
+        }else{
+        	return "false\n";
+        }
     }
     
     @GET
@@ -69,16 +56,26 @@ public class MyService {
     public String indexReachability(@PathParam("s") Long s, @PathParam("t") Long t){
         Long si = indexDb.mapping.get(s);
         Long ti = indexDb.mapping.get(t);
+        Node startScc = null;
+        Node endScc = null;
 
         // Get Start SCC node
-        result = indexDb.connection.execute("match (s) WHERE ID(s) = "+si+" return s");
-        ResourceIterator<Node> startSccResults = result.columnAs("s");
-        Node startScc = startSccResults.next();
+        try{
+	        result = indexDb.connection.execute("match (s) WHERE ID(s) = "+si+" return s");
+	        ResourceIterator<Node> startSccResults = result.columnAs("s");
+	        startScc = startSccResults.next();
+        }catch(RuntimeException e){
+        	return "Source: " + s + " ==> " + si + " NOT FOUND";
+        }
 
         // Get End SCC node
-        result = indexDb.connection.execute( "match (t) WHERE ID(t) = "+ti+" return t" );
-        ResourceIterator<Node> endSccResults = result.columnAs("t");
-        Node endScc = endSccResults.next();
+        try{
+	        result = indexDb.connection.execute( "match (t) WHERE ID(t) = "+ti+" return t" );
+	        ResourceIterator<Node> endSccResults = result.columnAs("t");
+	        endScc = endSccResults.next();
+	    }catch(RuntimeException e){
+	    	return "Target: " + t + " ==> " + ti + " NOT FOUND";
+	    }
         
         Set<Long> l_out_temp = IndexDB.fromString((String) startScc.getProperty("l_out"));
         Set<Long> l_in_temp = IndexDB.fromString((String) endScc.getProperty("l_in"));
@@ -100,12 +97,6 @@ public class MyService {
     @Path("/helloworld")
     public String helloWorld() {
         return "Hello World!\n";
-    }
-
-    @GET
-    @Path("/hello/{name}")
-    public String helloWorld(@PathParam("name") String name) {
-        return "Hello World2"+name+"!\n";
     }
 
 }
